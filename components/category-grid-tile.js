ca40f36cc5a48fcdb8576899a99f74ba3d92a611
Boopathy <kboopathyk7@gmail.com>
2022-12-25 17:35:28 +0530

    menu screen changes

---
+++ components/category-grid-tile.js
@@ -0,0 +1,54 @@
+import { Pressable, StyleSheet, Text, View, Platform } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View and style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.pressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.select({ ios: "visible", android: "hidden" }),
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
